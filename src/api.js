1e76b8e54d74dcec92d201bfae9d6ec9e5a5a199
priyaprabhu936 <asalappu143@gmail.com>
2025-08-26 21:48:25 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,7 +1,4 @@
 // src/api.js
-
-// Backend base URL (deploy pannadhu)
 const API_URL = "https://ott-backend1.onrender.com";
 
-// Default export, so App.js and other components can import easily
 export default API_URL;
