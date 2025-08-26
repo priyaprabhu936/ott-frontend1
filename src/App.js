bfc156d1ed53d97e61a12b11526cfad56bc7af74
priyaprabhu936 <asalappu143@gmail.com>
2025-08-26 16:57:05 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Login from "./components/Login";
 import Register from "./components/Register";
 import MovieGrid from "./components/MovieGrid";
-import "./App.css";
 
 function App() {
   return (
-    <div className="app">
-      <h1>🎬 CineStream</h1>
-      <Register />
-      <MovieGrid />
-    </div>
+    <Router>
+      <div>
+        {/* Navigation Bar */}
+        <nav style={{ padding: "10px", background: "#222", color: "white" }}>
+          <Link to="/" style={{ margin: "10px", color: "white" }}>
+            Home
+          </Link>
+          <Link to="/login" style={{ margin: "10px", color: "white" }}>
+            Login
+          </Link>
+          <Link to="/register" style={{ margin: "10px", color: "white" }}>
+            Register
+          </Link>
+        </nav>
+
+        {/* Routes */}
+        <Routes>
+          <Route path="/" element={<MovieGrid />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
