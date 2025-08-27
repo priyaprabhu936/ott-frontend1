4bdd9bd891ac67de02788a89f24b48aa26de52c6
priyaprabhu936 <asalappu143@gmail.com>
2025-08-27 11:37:30 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import Profile from "./pages/Profile";
+import Movies from "./pages/Movies";
 
 function App() {
   return (
@@ -11,12 +13,17 @@ function App() {
         <nav style={{ padding: "10px", borderBottom: "1px solid #444" }}>
           <Link to="/" style={{ marginRight: "15px", color: "#fff" }}>Home</Link>
           <Link to="/login" style={{ marginRight: "15px", color: "#fff" }}>Login</Link>
-          <Link to="/register" style={{ color: "#fff" }}>Register</Link>
+          <Link to="/register" style={{ marginRight: "15px", color: "#fff" }}>Register</Link>
+          <Link to="/profile" style={{ marginRight: "15px", color: "#fff" }}>Profile</Link>
+          <Link to="/movies" style={{ color: "#fff" }}>Movies</Link>
         </nav>
+
         <Routes>
+          <Route path="/" element={<h1 style={{ textAlign: "center" }}>🎬 Welcome to CineStream</h1>} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
-          <Route path="/" element={<h1 style={{ textAlign: "center" }}>🎬 Welcome to CineStream</h1>} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/movies" element={<Movies />} />
         </Routes>
       </div>
     </Router>
