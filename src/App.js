c3bd1b7620406088aca269a0242c8c43246d7458
priyaprabhu936 <asalappu143@gmail.com>
2025-08-27 23:01:56 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,16 @@
-// src/App.js
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
-import PrivateRoute from "./utils/PrivateRoute";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        
-        {/* Protected route */}
-        <Route
-          path="/dashboard"
-          element={
-            <PrivateRoute>
-              <Dashboard />
-            </PrivateRoute>
-          }
-        />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </Router>
   );
