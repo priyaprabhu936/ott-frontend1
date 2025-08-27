e813491220c5e3921b807b8b9a8aff3aa37aac2c
priyaprabhu936 <asalappu143@gmail.com>
2025-08-27 12:04:16 +0530

    Update PrivateRoute.js

--- src/utils/PrivateRoute.js
+++ src/utils/PrivateRoute.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Navigate } from "react-router-dom";
 
 function PrivateRoute({ children }) {
-  const token = localStorage.getItem("token"); 
+  const token = localStorage.getItem("token"); // login aprm token save aganum
   return token ? children : <Navigate to="/login" />;
 }
 
