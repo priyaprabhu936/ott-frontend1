3aaf198b75ef5fd267e5ced095886d4ded0c2c73
priyaprabhu936 <asalappu143@gmail.com>
2025-08-28 13:21:29 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Register from "./components/Register";
+import Login from "./components/Login";
 import Dashboard from "./components/Dashboard";
 
 function App() {
-  const isLoggedIn = !!localStorage.getItem("token");
-
   return (
     <Router>
       <Routes>
-        <Route path="/" element={isLoggedIn ? <Navigate to="/dashboard" /> : <Login />} />
+        <Route path="/" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/dashboard" element={isLoggedIn ? <Dashboard /> : <Navigate to="/" />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </Router>
   );
